b56180b16cb62cd916a9fea1fbd98c32fbe56c57
Quanhua <xinmuheart@gmail.com>
2024-05-15 09:26:49 +0800

    chore: code format;

--- packages/hardhat/test/Challenge0.ts
+++ packages/hardhat/test/Challenge0.ts
@@ -23,7 +23,7 @@ describe("🚩 Challenge 0: 🎟 Simple NFT Example 🤓", function () {
     it("Should deploy the contract", async function () {
       const EeeMay = await ethers.getContractFactory(contractArtifact);
       myContract = await EeeMay.deploy();
-      console.log("\t"," 🛰  Contract deployed on", await myContract.getAddress());
+      console.log("\t", " 🛰  Contract deployed on", await myContract.getAddress());
     });
 
     describe("mintItem()", function () {
